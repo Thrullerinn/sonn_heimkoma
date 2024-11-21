082724d560add6bb84d627db2603eb56960ec66c
Thrullerinn <throstur@trackwell.com>
2024-11-21 17:41:26 +0000

    sida css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -75,7 +75,7 @@ class NavBar extends Component{
                                             <li><Link to={`${process.env.PUBLIC_URL}/`}>Heim</Link> </li>
 
                                             <li className="has-children has-children--multilevel-submenu">
-                                                <Link to={`${process.env.PUBLIC_URL}/order`}>Bækurnar</Link>
+                                                <Link to={`${process.env.PUBLIC_URL}/sonnheimkomabok`}>Bækurnar</Link>
                                                 <ul className="submenu">
                                                     <li><Link to={`${process.env.PUBLIC_URL}/sonnbok`}>Sönn</Link></li>
                                                     <li><Link to={`${process.env.PUBLIC_URL}/sonnheimkomabok`}>Sönn heimkoma</Link></li>
