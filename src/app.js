51bfe95c192434f968d93544842b66d3a91587ff
Thrullerinn <throstur@trackwell.com>
2024-11-24 20:27:29 +0000

    hash router

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import React, {  Suspense, lazy } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import WhoAreWe from "./home/WhoAreWe.js";
+import { Routes, Route } from "react-router-dom";
 import SonnHomePage from "./home/SonnHomePage.js";
 import SonnHeimkomaBook from "./home/SonnHeimkomaBook.js";
 import SonnBook from "./home/SonnBook.js";
 import AuthorPage from "./home/AuthorPage.js"
+import { HashRouter } from 'react-router-dom'
 
 const Sonn = lazy(() => import("./home/Sonn"));
 const Contact = lazy(() => import("./pages/Contact"));
@@ -12,7 +12,7 @@ const NoMAtch = lazy(() => import("./pages/404"));
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
         <Suspense fallback={<div />}>
             <Routes>
                 <Route path="/" element={<SonnHomePage />} />
@@ -27,7 +27,7 @@ const App = () => {
                 <Route path="*" element={<NoMAtch />} />
             </Routes>
         </Suspense>
-      </Router>
+      </HashRouter>
   )
 }
 
